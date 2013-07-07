f56d65f9544b8549dfaf6b704ef4cec8b40da46c
MakerSquare Student <students@themakersquare.com>
2013-07-07 16:12:13 -0500

    fixed create_recipe method so it is using the attr_accesor correctly. Previously - I had a misunderstanding of how class instances were working.

--- SmoothieLand_Ruby.rb
+++ SmoothieLand_Ruby.rb
@@ -9,19 +9,9 @@
 #                                     }]
 #           ]
 
-class Recipe 
-
-  attr_accessor :servings, :base, :ingredients
-
-  @@all_recipes = []
-
-  def initialize
-    puts "we are going through creating a new recipe"
-    self.servings = gets.chomp.to_i  # stopped here Tuesday (7/2)
-
-    @@all_recipes << self
-  end
 #-------Welcome Prompt 
+  
+class Recipe 
   def self.execute
     puts "Welcome to SmoothieLand!" 
     puts "Options press: 1)To enter new 'Jackpot' recipe. 2)To see instructions"
@@ -34,29 +24,43 @@ class Recipe
   end
 
   def Recipe.give_instructions
-    puts "Are you new to making your own smoothie recipes? Is it driving you bonkers trying to find good ingredient combinations? And when you hit a 'jackpot' recipe, don't you feel so good and want to share it? That's what SmootieLand is here to do: make it easy for you to share those recipes and then share them on our website and through Twitter." 
+    puts "Are you new to making your own smoothie recipes? Is it driving you bonkers trying to find good ingredient combinations? And when you hit a 'jackpot' recipe, don't you feel so good and want to share it? That's what SmootieLand is here to do: make it easy for you to share those 'gem' recipes." 
     puts "Ready? Hit enter key."
     gets
     x = Recipe.new 
   end
+#-------User creating new recipes
+  attr_accessor :servings, :base, :ingredients # automatically gives you the keys for an instance you will create.
+  
+  @@all_recipes = []
+  
+  def initialize
+    puts "Okay, ready to upload your recipe? \n (Hit enter key to start)"
+    gets
+    create_recipe
+  end
+  
+  
+    # self.servings = gets.chomp.to_i  # stopped here Tuesday (7/2)
+    # servings = gets.chomp
+    # @@all_recipes << servings
 
+  # @new_recipe = {servings: "", base: "", ingredients:[]} - Doesn't work when I try to add servings value from line 57
 
-
-
-
-
-  @new_recipe = {:servings => 0, :base => "", :ingredients => []}
+  
 
   def create_recipe
     puts "\n"
     puts "Please enter the number of servings your smoothie recipe makes."
-    @answer_servings = gets.chomp.to_i
-    @new_recipe[:servings] = @answer_servings
-
+    answer_servings = gets.chomp.to_i
+    puts "Cool, you entered #{answer_servings} servings. \n (Hit enter to conitnue)"
+    gets
+    self.servings = answer_servings # self is an instance (in this case)
     puts "Is it a juice-based or milk-based smoothie?"
-    answer_base=gets
-    @new_recipe[:base] = answer_base
+    answer_base = gets.chomp
+    self.base = answer_base
   end
+
 end 
 
 Recipe.execute  
